ab2610ab9439a770e81bab8bb47294362d9b34f9
jdgonzalez03 <juandavidgonzalezurrego@gmail.com>
2026-02-10 18:06:18 -0500

    feat: add summary section

---
+++ src/components/cv/SummarySection.tsx
@@ -0,0 +1,24 @@
+import { useCvActions } from "../../hooks/useCvActions";
+import { useAppSelector } from "../../hooks/useStore";
+import { AccordionSection } from "../common/AccordionSection";
+import { TextArea } from "../common/TextArea";
+import { SummaryIcon } from "../Icons";
+
+export function SummarySection() {
+	const { summary } = useAppSelector((state) => state.cv);
+	const { updateSummary } = useCvActions();
+	const handleSummaryChange = (value: string) => {
+		updateSummary({ text: value });
+	};
+	return (
+		<AccordionSection title="Summary" icon={<SummaryIcon />}>
+			<div className="p-4">
+				<TextArea
+					value={summary.text}
+					onChange={handleSummaryChange}
+					placeholder="Briefly describe your career and goals..."
+				/>
+			</div>
+		</AccordionSection>
+	);
+}
